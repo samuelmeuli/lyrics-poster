edb15043412ccac34c875b4f63a92879fa2fa334
Samuel Meuli <dev@samuelmeuli.com>
2018-07-02 16:30:36 +0200

    Remove unnecessary Webpack config attribute

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-		publicPath: '/'
+		filename: 'bundle.js'
 	},
 	module: {
 		rules: [
